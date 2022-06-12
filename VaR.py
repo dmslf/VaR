3da5ab475d274749bc422a502b26c5d65bb34fdd
dmslf <86357946+dmslf@users.noreply.github.com>
2022-06-12 10:00:36 +0200

    Add files via upload

---
+++ VaR.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 12 09:39:09 2022
+
+@author: fusiek
+"""
+
+#Shows daily VaR in terms of % loss for a single security for pre-defined confidence level and period of time
+
+import pandas as pd
+
+ticker = input("Ticker: ")
+range = int(input("Range: "))
+confidence = int(input("Confidence: "))
+
+
+df = pd.read_csv(ticker+'.txt', index_col="<DATE>", parse_dates=True)
+
+df["Return"] = df['<CLOSE>'].pct_change()
+
+
+VaR = df['Return'].tail(range).quantile(1-confidence/100)
+
+print("The loss will not exceed "+str(VaR*100)+"% with a "+str(confidence)+"% confidence.")
+
